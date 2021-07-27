9641b7cd5d22f7499def16c36d8808181fee6283
ayush-abq <85103602+ayush-abq@users.noreply.github.com>
2021-07-27 21:28:29 +0530

    Add files via upload

---
+++ Day 19/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Chess from './Chess';
+// import Card from './Card';
+import './Chess.css';
+
+
+function App() {
+    return(
+        <div className="container">
+          <h1>Chess Board </h1>
+          <div>
+          <Chess/>
+          </div>
+        </div>
+    );
+}
+export default App;
